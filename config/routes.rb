6573d01d26a1c5190170c00058060167f7c78995
derek-tor <derekmuoktor@gmail.com>
2013-11-08 22:49:17 -0800

    added main index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Munch::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'main#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
